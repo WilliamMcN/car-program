9765bc28c0068666e2c4065947f2f886a9ec0f8e
danie <danie@10.250.5.203>
2017-02-23 12:53:52 -0500

    new validations and updated toSting()

--- src/javaapplication10/Car.java
+++ src/javaapplication10/Car.java
@@ -18,10 +18,23 @@ public class Car {
     private String make,model,list_of_features;
     private int year;
     private double selling_price,mileage;
+    private ArrayList<String> makes = new ArrayList<String>();
+    private ArrayList<String> mods = new ArrayList<String>();
+    
 
     public Car(String make, String model, String list_of_features, int year, double selling_price, double mileage) {
-        this.make = make;
-        this.model = model;
+        this.makesFiller();
+        this.modsFiller();
+        //make
+        if(makes.contains(make))
+            this.make = make;
+        else
+            throw new IllegalArgumentException("That is not a vaild make");
+        //model
+        if(mods.contains(model))
+            this.model = model;
+        else
+            throw new IllegalArgumentException("Not a vaild model");
         //year
         if((year>1800) && (year<(LocalDate.now().getYear())+1))
             this.year = year;
@@ -95,11 +108,34 @@ public class Car {
     }
     
     /**
-     * this method will return the class as a string in the form 
+     * This method will return the class as a string in the form 
      * @return 
      */
     @Override
     public String toString(){
-        return (make+" "+model+" "+" "+year+" "+mileage+" "+this.selling_price+" "+this.list_of_features);
+        return make+" "+model+" "+year+" "+mileage;
+    }
+    
+    /**
+     * This method will populate an arraylist with car makes
+     */
+    private void makesFiller(){
+        makes.add("Chevrolet");
+        makes.add("Jeep");
+        makes.add("Ford");
+        makes.add("Dodge");
+        makes.add("Hyundai");
+    }
+    /**
+     * This method will populate an arraylist with models
+     */
+    private void modsFiller(){
+        mods.add("Ranger");
+        mods.add("TJ");
+        mods.add("Madza 5");
+        mods.add("Civic");
+        mods.add("CR-v");
+        mods.add("Corvette");
+        mods.add("F-150");
     }
 }
